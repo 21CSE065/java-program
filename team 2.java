b47177c9d834fecf84b6a2927900297fc23ce730
21CSE065 <111368612+21CSE065@users.noreply.github.com>
2022-08-16 11:08:51 -0700

    Add files via upload

---
+++ team 2.java
@@ -0,0 +1,166 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oops1;
+
+import java.util.Scanner;
+public class oops1 {
+    public static void main(String[] args) {
+        int a,b,minimum;
+        Scanner obj=new Scanner(System.in);
+        System.out.ptintln("enter first number");
+        a=obj.nextInt();
+        System.out.println("enter second number");
+        b=obj.nextInt();
+        if (a>b)
+           System.out.println("a is the minimum number");
+        else
+            System.out.println("b is the minimum number");
+        
+    }
+    
+}
+
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sumproduct;
+
+import java.util.Scanner;
+public class sumproduct {
+    public static void main(String[] args) {
+      Scanner obj=new Scanner(System.in);
+      int a,b,c,d;
+      System.out.println("enter a:");
+      a=obj.nextInt();
+      System.out.println("enter b:");
+      b=obj.nextInt();
+      c=a+b;
+      d=a*b;
+      System.out.println("the sum is:...."+c);
+      System.out.println("the product is:...."+d);  
+    }
+}
+
+
+
+
+import java.util.scanner;
+public class dec;
+public static void main(String ar[])
+{
+for(int i=1;i<=n;i++)
+{
+for(int j=i;j<=n;j++)
+{
+System.out.println(" ");
+}
+for(int j=1;j<=i;j++)
+{
+System.out.println("*");
+}
+System.out.println();
+}
+}
+}
+
+
+
+
+import java.util.scanner;
+public class greet
+{
+public static void main(String[] args)
+{
+Scanner obj=new Scanner(System.in);
+int m1,m2,m3,m4,m5,avg;
+System.out.println("enter m1,m2,m3,m4,m5 from 1-10");
+m1=obj.nextInt();
+m2=obj.nextInt();
+m3=obj.nextInt();
+m4=obj.nextInt();
+m5=obj.nextInt();
+avg=(m1+m2+m3+m4+m5)/5;
+switch(avg)
+{
+m1=obj.nextInt();
+m2=obj.nextInt();
+m3=obj.nextInt();
+m4=obj.nextInt();
+m5=obj.nextInt();
+avg=(m1+m2+m3+m4+m5)/5;
+switch(avg)
+{
+case10:
+System.out.println("excellent");
+break;
+case9:
+System.out.println("very good");
+break;
+case8:
+System.out.println("very good");
+break;
+case7:
+System.out.println("good");
+break;
+case6:
+System.out.println("good");
+break;
+case5:
+System.out.println("poor");
+break;
+case4:
+System.out.println("poor");
+break;
+case3:
+System.out.println("poor");
+break;
+case2:
+System.out.println("poor");
+break;
+case1:
+System.out.println("very poor");
+break;
+case0:
+System.out.println("very poor");
+break;
+default;
+System.out.println("invalid");
+break;
+}
+}
+}
+
+
+
+
+import java.util.scanner;
+public class monday
+{
+public static void main(String[] args)
+{
+Scanner obj=new Scanner(System.in);
+int n,month,day;
+System.out.println("enter n:");
+n=obj.nextInt();
+month=n//30;
+day=n%30;
+System.out.println(n+"days="+month.."months and"+day..+"days");
+}
+}
+
+
+
+
+
+
+
+
+
